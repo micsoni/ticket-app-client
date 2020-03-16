af03db6b7e432f3c0abbeeaec391cc2a863ea965
micsoni <micsoni@gmail.com>
2020-03-16 23:11:27 +0100

    added navbar

--- src/App.js
+++ src/App.js
@@ -4,13 +4,12 @@ import { Route } from "react-router-dom";
 import store from "./store/index";
 import LoginFormContainer from "./components/LoginFormContainer";
 import SignupFormContainer from "./components/SignupFormContainer"
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <Provider store={store}>
-    <div>
-     <p>Hello</p>
-    </div>
+    <Navbar/>
     <Route exact path="/login" component={LoginFormContainer} />
     <Route exact path="/signup" component={SignupFormContainer} />
     </Provider>
---
+++ src/components/Navbar.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-md navbar-dark bg-info">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <Link to="/" className="nav-link">
+                Home 
+              </Link>
+            </li>
+            <li className="nav-item active">
+              <Link to="/profile" className="nav-link">
+                My profile
+              </Link>
+            </li>
+            <li className="nav-item active">
+              <Link to="/login" className="nav-link">
+                Login
+              </Link>
+            </li>
+            <li className="nav-item active">
+              <Link to="/signup" className="nav-link">
+                Signup
+              </Link>
+            </li>
+            <li className="nav-item active">
+              <Link to="/events" className="nav-link">
+                All events
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
